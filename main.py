e4507f8a0cd0f85a8d245f85a0f1b08c9a124dc9
Jake Van Dine <jacob213vandine@gmail.com>
2023-05-08 17:50:37 -0400

    running with charles

--- main.py
+++ main.py
@@ -10,7 +10,17 @@ DANCES = {}
 NINJAS = []
 SCENES = []
 
+
+def get_data():
+    dance_df = pd.read_excel("./test/dance.xlsx")
+    cast_df = pd.read_excel("./test/cast.xlsx")
+    dance_df.head()
+    cast_df.head()
+
 def display():
     return
 
+
+if __name__ == "__main__":
+    get_data() 
 
