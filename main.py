d97d02047e1e2731ea0cfe475026b6188397a4d0
Jake Van Dine <jacob213vandine@gmail.com>
2023-05-09 14:13:42 -0400

    stopping point for now

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 import os
 import pandas as pd
 import time
-import member
-import dance
 import scene
 
-CAST = {}
-DANCES = {}
+CAST = dict()
+DANCES = dict()
 NINJAS = []
 SCENES = []
 
 
-def get_data():
-    dance_df = pd.read_excel("./test/dance.xlsx")
-    cast_df = pd.read_excel("./test/cast.xlsx")
-    dance_df.head()
-    cast_df.head()
+def gen_data():
+    dance_df = pd.read_excel("./22-23_ALL_PEOPLE_INVOLVED.xlsx", "Dances")
+    dance_df = dance_df.dropna().drop()
+    cast_df = pd.read_excel("./22-23_ALL_PEOPLE_INVOLVED.xlsx", "Cast")
+
+    for (col_name, col_data) in dance_df.iteritems():
+        DANCES[col_name] = col_data
+
+
+
 
 def display():
     return
 
 
 if __name__ == "__main__":
-    get_data() 
+    gen_data() 
+    print(DANCES)
 
--- scene.py
+++ scene.py
@@ -4,6 +4,11 @@
 class scene:
 
     def __init__(self, cast, dance) -> None:
-        pass
+        self.cast = cast
+        self.dance = dance
+        self.members = self.getMembers()
+
+    def getMembers(self):
+        pass 
 
     
